974fc4e5de4d8769580d9933ec22824bfe7c20fb
TheVu <thevudoan@gmail.com>
2017-10-09 22:16:21 +0700

    Add api/rooms + libs

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config.json');
-const imageApi = require('./modules/api/members/usersController');
-const staffApi = require('./modules/api/staffs/staffsController');
-const postApi = require('./modules/api/posts/postsController');
+const imageApi = require('./modules/api/users/usersController');
+// const staffApi = require('./modules/api/staffs/staffsController');
+const roomApi = require('./modules/api/rooms/roomsController');
 
 var app = express();
 
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended : true}));
 
 app.use('/api/users', imageApi);
 app.use('/api/staffs', imageApi);
-app.use('/api/post', imageApi);
+app.use('/api/room', imageApi);
 
 mongoose.connect(config.connectionString, (err) => {
   if (err) {
@@ -23,8 +23,8 @@ mongoose.connect(config.connectionString, (err) => {
   } else {
     console.log('Connect to db success');
   }
-})
+});
 
 app.listen(config.port , () => {
   console.log(`App listen on ${config.port}`);
-})
+});
